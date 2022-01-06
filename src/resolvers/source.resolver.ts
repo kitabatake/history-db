5b81bdc49da4daac4baa11e226f65d24b259fabd
Tatsuya Kitabatake <e2tod2@gmail.com>
2022-01-06 17:31:57 +0900

    source update mutation

--- src/resolvers/source.resolver.ts
+++ src/resolvers/source.resolver.ts
@@ -1,10 +1,20 @@
 import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { PrismaService } from '../prisma.service';
 import { Source } from '../models/source.model';
+import { Person } from '../models/person.model';
 
 @Resolver(() => Source)
 export class SourceResolver {
   constructor(private prisma: PrismaService) {}
+
+  @Query(() => Source)
+  async source(@Args('id', { type: () => Int }) id: number): Promise<Source> {
+    return this.prisma.source.findUnique({
+      where: {
+        id: id,
+      },
+    });
+  }
 
   @Query(() => [Source])
   async sources(
@@ -28,6 +38,21 @@ export class SourceResolver {
   @Mutation(() => Source)
   async createSource(@Args({ name: 'name' }) name: string) {
     return this.prisma.source.create({
+      data: {
+        name: name,
+      },
+    });
+  }
+
+  @Mutation(() => Source)
+  async updateSource(
+    @Args({ name: 'id', type: () => Int }) id: number,
+    @Args({ name: 'name' }) name: string,
+  ) {
+    return this.prisma.source.update({
+      where: {
+        id: id,
+      },
       data: {
         name: name,
       },
